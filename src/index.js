8135683c76804e39875d4f143d70babd1a4f5da7
vali <v.naeimabadi@gmail.com>
2021-11-25 11:24:56 +0330

    refactor css path to assets folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "./css/style.css";
+import "./assets/css/style.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
